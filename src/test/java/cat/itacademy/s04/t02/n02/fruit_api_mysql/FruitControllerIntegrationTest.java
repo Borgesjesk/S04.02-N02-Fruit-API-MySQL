dd9b738920282ba4d8f8246e1615133ab3c6e694
BORGES <jessborgesb@gmail.com>
2026-04-29 12:19:22 +0200

    test: add FruitController integration tests with H2

--- src/test/java/cat/itacademy/s04/t02/n02/fruit_api_mysql/FruitControllerIntegrationTest.java
+++ src/test/java/cat/itacademy/s04/t02/n02/fruit_api_mysql/FruitControllerIntegrationTest.java
@@ -1,11 +1,12 @@
 package cat.itacademy.s04.t02.n02.fruit_api_mysql;
 
+import cat.itacademy.s04.t02.n02.fruit_api_mysql.dto.FruitRequestDto;
+import cat.itacademy.s04.t02.n02.fruit_api_mysql.dto.FruitResponseDto;
 import cat.itacademy.s04.t02.n02.fruit_api_mysql.dto.ProviderRequestDto;
-import cat.itacademy.s04.t02.n02.fruit_api_mysql.repository.FruitRepository;
-import cat.itacademy.s04.t02.n02.fruit_api_mysql.repository.ProviderRepository;
-import cat.itacademy.s04.t02.n02.fruit_api_mysql.service.FruitService;
-import cat.itacademy.s04.t02.n02.fruit_api_mysql.service.ProviderService;
-import org.junit.jupiter.api.MediaType;
+import cat.itacademy.s04.t02.n02.fruit_api_mysql.dto.ProviderResponseDto;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
@@ -13,13 +14,16 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
-@ActiveProfiles("tests")
 @Transactional
 class FruitControllerIntegrationTest {
 
@@ -29,24 +33,111 @@ class FruitControllerIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
-    private FruitRepository fruitRepository;
 
-    @Autowired
-    private FruitService fruitService;
-
-    @Autowired
-    private ProviderRepository providerRepository;
-
-    @Autowired
-    private ProviderService providerService;
-
-    private Long createTestProvider() throws Exception {
-        ProviderRequestDto provider = new ProviderRequestDto();
+    private Long createTestProvider(String name, String country) throws Exception {
+        ProviderRequestDto provider = new ProviderRequestDto(name, country);
         MvcResult result = mockMvc.perform(post("/providers")
-                .contentType(String.valueOf(MediaType.APPLICATION_JSON))
-                .content(objectMapper.writeValueAsString(provider)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(provider)))
                 .andReturn();
 
+        ProviderResponseDto response = objectMapper.readValue(
+                result.getResponse().getContentAsString(), ProviderResponseDto.class);
+        return response.getId();
+    }
 
+    private FruitResponseDto createTestFruit(Long providerId, String name, int weightInKilos) throws Exception {
+        FruitRequestDto fruit = new FruitRequestDto(providerId, name, weightInKilos);
+        MvcResult result = mockMvc.perform(post("/fruits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(fruit)))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        return objectMapper.readValue(
+                result.getResponse().getContentAsString(), FruitResponseDto.class);
+
+    }
+
+    @Test
+    @DisplayName("POST: Create a valid fruit linked to provider")
+    void createValidFruitLinkedToProvider() throws Exception {
+        Long providerId = createTestProvider("Supplier ABC", "Spain");
+        FruitRequestDto fruitDto = new FruitRequestDto(providerId, "Banana", 1);
+
+        mockMvc.perform(post("/fruits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(fruitDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("Banana"))
+                .andExpect(jsonPath("$.weightInKilos").value(1))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("GET ALL: Retrieve all fruits in isolated state")
+    void getAllFruits() throws Exception {
+        Long providerId = createTestProvider("General Supplier", "Spain");
+        createTestFruit(providerId, "Kiwi", 1);
+        createTestFruit(providerId, "Apple", 2);
+
+        mockMvc.perform(get("/fruits"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[?(@.name == 'Kiwi')]").exists())
+                .andExpect(jsonPath("$[?(@.name == 'Apple')]").exists());
+    }
+
+    @Test
+    @DisplayName("GET BY ID: Find specific fruit")
+    void getFruitById() throws Exception {
+        Long providerId = createTestProvider("ID test supplier", "Spain");
+        FruitResponseDto response = createTestFruit(providerId, "Mango", 4);
+
+        mockMvc.perform(get("/fruits/{id}", response.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("Mango"));
+    }
+
+    @Test
+    @DisplayName("PUT: Update fruit name and weight in Kilos")
+    void updateFruitNameAndWeightInKilos() throws Exception {
+        Long providerId = createTestProvider("Update Supplier", "Spain");
+        FruitResponseDto response = createTestFruit(providerId, "Old Fruit", 4);
+        FruitRequestDto updateDto = new FruitRequestDto(providerId, "New Fruit", 10);
+
+        mockMvc.perform(put("/fruits/{id}", response.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("New Fruit"))
+                .andExpect(jsonPath("$.weightInKilos").value(10));
+    }
+
+    @Test
+    @DisplayName("DELETE: Remore fruit and verify 404")
+    void deleteFruitNotFound() throws Exception {
+        Long providerId = createTestProvider("Delete Supplier", "Spain");
+        FruitResponseDto response = createTestFruit(providerId, "Disposable Fruit", 4);
+
+        mockMvc.perform(delete("/fruits/{id}", response.getId()))
+                .andExpect(status().isNoContent());
+
+        mockMvc.perform(get("/fruits/{id}", response.getId()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("GIVEN an invalid Provider ID WHEN creating fruit THEN return 404")
+    void createInvalidProviderId() throws Exception {
+        FruitRequestDto fruitDto = new FruitRequestDto(999L, "OOFruit", 3);
+
+        mockMvc.perform(post("/fruits")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(fruitDto)))
+                .andExpect(status().isNotFound());
+
+    }
 }
